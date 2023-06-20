3c76f798d3558af2e70e3a49d9c6366bbcea3b18
Donald KANTI <donaldkant2000@gmail.com>
2023-06-20 10:39:23 +0100

    added some change

--- app/lib/services/rest.config.dart
+++ app/lib/services/rest.config.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class APIEndpoints {
   static String localApiBaseUrl = "http://192.168.91.39:1337/api";
-  static String networkApiBaseUrl = "http://192.168.91.39:1337/api";
+  static String onlineApiBaseUrl = "http://ecc-hymns-cms.metatronicx.io/api";
   static String baseUrl = localApiBaseUrl;
   static final String gounHymnsUrl = "$baseUrl/goun-hymns";
   static final String frenchHymnsUrl = "$baseUrl/french-hymns";
